c4c75df6b71cae6f74190525b940991f9c0d6154
baochenhu <baochenhu@gmail.com>
2014-10-03 16:33:34 -0400

    edit filter.js

--- filter.js
+++ filter.js
@@ -1,7 +1,12 @@
   function getShortMessages(messages) {
       // SOLUTION GOES HERE
-    
-         console.log(messages)
+        var temp=messages.map(function map(arrElement){
+                return arrElement['message'];
+        });
+        var result=temp.filter(function filter(element){
+        	return element.length<50
+        });
+        return result;
     }
     
     module.exports = getShortMessages
